ecf1317062a2d3bba97ec844d0443cf93106d4d6
Saimon Leandro de Sousa Tavares <48137085+saimontavares@users.noreply.github.com>
2025-09-09 20:48:05 +0000

    configurado ESLINT e Prettier

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["src/generated/", "node_modules/", "dist/"],
+  },
+  { files: ["src/**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "prefer-const": "warn"
+    }
+  }
+]);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,17 @@
-import express, { Request, Response } from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const valor => 1;
 
 const app = express();
-const PORT = process.env.PORT || 3880;
+const PORT = process.env.PORT ?? 7788;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Olá 20!');
+app.get("/", (req, res) => {
+  res.send("Olá, bem-vindo(a) ao curso de PW2!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`App running on port ${PORT}.`);
 });
