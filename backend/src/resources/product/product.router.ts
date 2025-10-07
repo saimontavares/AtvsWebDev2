8439f02c6b9eb0a7918af8a5bd0f9144ecc06006
Saimon Leandro de Sousa Tavares <48137085+saimontavares@users.noreply.github.com>
2025-10-07 20:28:29 +0000

    adicionando checkAuth middleware

--- backend/src/resources/product/product.router.ts
+++ backend/src/resources/product/product.router.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import productController from "./product.controller";
 import { productSchema } from "./product.schema";
 import { validate } from "../../middlewares/validate";
+import {checkAuthorization} from "../../middlewares/checkAuthorization";
 
 const router = Router();
 
 router.get("/", productController.index);
-router.post("/", validate(productSchema), productController.create);
+router.post("/", checkAuthorization, validate(productSchema), productController.create);
 router.get("/:id", productController.read);
-router.put("/:id", productController.update);
-router.delete("/:id", productController.remove)
+router.put("/:id", checkAuthorization, productController.update);
+router.delete("/:id", checkAuthorization, productController.remove);
 
 export default router;
