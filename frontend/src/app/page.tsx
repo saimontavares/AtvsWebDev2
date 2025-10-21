f7c7fe8d7ddbbadbb7d94aef52b045137dd322cc
Saimon Leandro de Sousa Tavares <48137085+saimontavares@users.noreply.github.com>
2025-10-21 03:59:54 +0000

    page pegar produtos consertado

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import ProductList from "@/views/products/list/ProductList"
 import { ProductDto } from "@/views/products/Product.types"
 
 async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/products`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DOCKER_API}/product`)
   const products : ProductDto[] = await res.json()
   console.log(products)
   return <ProductList products={products} />
