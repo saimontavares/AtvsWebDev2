c6aa152edc320ec51e29a6fe51be8922d7b67b6b
Saimon Leandro de Sousa Tavares <saimonn.sousa.tavares@gmail.com>
2025-12-03 15:24:08 -0400

    colocando o preço nos card da lista de produtos

--- frontend/src/views/products/list/ProductCard.tsx
+++ frontend/src/views/products/list/ProductCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "flowbite-react";
-import { memo, useContext, useState } from "react";
+import { memo, useContext } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import styles from "../Product.module.css";
 import { ProductDto } from "../Product.types";
@@ -25,6 +25,9 @@ function ProductCard({ product }: ProductCardProps) {
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         <Link href={`/product/${product.id}`}>{product.name}</Link>
       </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        <b>Preço:</b> {product.price}
+      </p>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         {product.description}
       </p>
